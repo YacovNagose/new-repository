fbcaeec0f8f4c740cbc43dad712d58943083f6b3
YacovNagose <54777739+YacovNagose@users.noreply.github.com>
2019-09-07 14:08:39 +0300

    Update exercise4-fix_server1.sh

--- vagrant/fixScripts/exercise4-fix_server1.sh
+++ vagrant/fixScripts/exercise4-fix_server1.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 #add fix to exercise4-server1 here
-Add the name and ip's of the servers on /etc/host
-on server1
+Add the name and ip's of the server2 on /etc/host
 192.168.100.11 server2
 
-on server2
-192.168.100.10 server1
+
